a42f16852e0765be8806f2d4b31b30bec193194c
Alex Shih <alex.alex.shih@gmail.com>
2019-08-28 22:52:36 -0400

    added deck class

---
+++ deck.js
@@ -0,0 +1,28 @@
+import Card from "./card.js";
+const _ = require("lodash");
+
+class Deck {
+  constructor() {
+    this.suits = ["CLUBS", "SPADES", "DIAMONDS", "HEARTS"];
+    this.values = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+    this.cards = [];
+    this.suits.forEach(suit => {
+      this.values.forEach(value => {
+        const card = new Card(suit, value);
+        this.cards.push(card);
+      });
+    });
+  }
+
+  shuffle() {
+    _.shuffle(this.cards);
+  }
+
+  deal() {
+    return this.cards.pop();
+  }
+
+  get cards() {
+    return this.cards;
+  }
+}
